c410211dd4630049769ec1ff9d066f312fb75b03
unknown <manahilshaikh8551@gmail.com>
2023-02-24 21:16:50 +0530

    html

---
+++ map filter.js
@@ -0,0 +1,10 @@
+const mobile=[
+    {name:"samsung Galaxy s20",rate:"1000",color:"black"},
+    {name:"samsung Guru",rate:"2000",color:"golden"},
+    {name:"POCO C50",rate:"2000",color:"blue"},
+    {name:"APPLE iphone 14",rate:"5000",color:"black"},
+    {name:"MotorolaG62",rate:"3000",color:"Gray"},
+    {name:"I KALL K60",rate:"9000",color:"green"},
+    {name:"REDMI 9i",rate:"8000",color:"Blue"},
+    {name:"POCO C31",rate:"8000",color:"Gray"},
+];
